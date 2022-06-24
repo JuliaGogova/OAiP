1d080579aa3c43ea1fff136b43c1eb8f9dc0a364
JuliaGogova <105663885+JuliaGogova@users.noreply.github.com>
2022-06-24 16:25:55 +0300

    Add files via upload

---
+++ 14.06 & 21.06/1406/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1406
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] login = new string[3] { "Prodavec", "StProdavec", "Admin" };
+            string[] password = new string[3] { "Prodavec", "StProdavec", "Admin" };
+
+            if (LogginBox.Text.Equals(login[0]) & PasswordBox.Password.Equals(password[0]))
+            {
+                MessageBox.Show("Вы зашли за продовца");
+                Prodawec winprodawec = new Prodawec();
+                winprodawec.Show();
+                Close();
+            }
+            else if (LogginBox.Text.Equals(login[1]) & PasswordBox.Password.Equals(password[1]))
+            {
+                MessageBox.Show("Вы зашли за сменного");
+                Smena winsmena = new Smena();
+                winsmena.Show();
+                Close();
+            }
+            else if (LogginBox.Text.Equals(login[2]) & PasswordBox.Password.Equals(password[2]))
+            {
+                MessageBox.Show("Вы зашли за Администратора");
+                Admin winAdmin = new Admin();
+                winAdmin.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Введены неправвильные данные");
+            }
+        }
+    }
+}
